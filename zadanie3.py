eea73ba35c1f10496edabbe5ec3a78998d6f3d5e
KamilC13 <kciask@student.agh.edu.pl>
2026-03-30 21:52:06 +0200

    Update zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -1,6 +1,19 @@
-total = sum([x for x in range(1_000_000) if x % 3 == 0])
-print("Suma:", total)
+#Cel: Obliczenie sumy oraz znalezienie największej wartości
+#dla sześcianów (x**3) liczb podzielnych przez 5.
+numbers = range(100_000)
+cubes_list = []
 
-#Zrefaktoryzuj kod zgodnie z dobrymi praktykami przekazywania generatorów.
-#Przekaż generator bezpośrednio do funkcji sum(), usuwając nawiasy kwadratowe.
-#Sprawdź, czy wynik po refaktoryzacji pozostał ten sam.
+for x in numbers:
+    if x % 5 == 0:
+        cubes_list.append(x**3)
+
+total_sum = sum(cubes_list)
+max_value = max(cubes_list)
+
+print("Suma:", total_sum)
+print("Największa wartość:", max_value)
+
+#Zrefaktoryzuj powyższy kod, całkowicie usuwając pętlę 'for' oraz listę 'cubes_list', która niepotrzebnie zajmuje pamięć[cite: 360].
+#Oblicz 'total_sum', wpisując wyrażenie generatorowe z odpowiednim warunkiem 'if' bezpośrednio do funkcji sum() (pamiętaj o braku nawiasów kwadratowych!)[cite: 360].
+#W analogiczny sposób oblicz 'max_value', wpisując wyrażenie generatorowe prosto do funkcji max()[cite: 360].
+#Uruchom kod i upewnij się, że wyniki wypisywane na konsoli pozostały takie same.
