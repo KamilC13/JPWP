909c4487a5eadb5e9563ac5dd144b35089d4c2df
KamilC13 <kciask@student.agh.edu.pl>
2026-03-30 21:54:44 +0200

    Update zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -11,7 +11,7 @@ max_value = max(cubes_list)
 print("Suma:", total_sum)
 print("Największa wartość:", max_value)
 
-#Zrefaktoryzuj powyższy kod, całkowicie usuwając pętlę 'for' oraz listę 'cubes_list', która niepotrzebnie zajmuje pamięć[cite: 360].
-#Oblicz 'total_sum', wpisując wyrażenie generatorowe z odpowiednim warunkiem 'if' bezpośrednio do funkcji sum() (pamiętaj o braku nawiasów kwadratowych!)[cite: 360].
-#W analogiczny sposób oblicz 'max_value', wpisując wyrażenie generatorowe prosto do funkcji max()[cite: 360].
+#Zrefaktoryzuj powyższy kod, całkowicie usuwając pętlę 'for' oraz listę 'cubes_list', która niepotrzebnie zajmuje pamięć.
+#Oblicz 'total_sum', wpisując wyrażenie generatorowe z odpowiednim warunkiem 'if' bezpośrednio do funkcji sum() (pamiętaj o braku nawiasów kwadratowych!).
+#W analogiczny sposób oblicz 'max_value', wpisując wyrażenie generatorowe prosto do funkcji max().
 #Uruchom kod i upewnij się, że wyniki wypisywane na konsoli pozostały takie same.
