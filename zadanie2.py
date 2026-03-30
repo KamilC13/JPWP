3a6bb313aac7d951317015f3f8e2d5ff1d264ede
KamilC13 <kciask@student.agh.edu.pl>
2026-03-30 21:38:24 +0200

    Add files via upload

---
+++ zadanie2.py
@@ -0,0 +1,11 @@
+#zadanie2.py
+import sys
+
+#Lista zawierająca kwadraty liczb od 0 do 9999
+squares_list = [x**2 for x in range(10000)]
+print("Rozmiar listy w pamięci (bajty):", sys.getsizeof(squares_list))
+
+#Zrefaktoryzuj powyższy kod używając skróconej składni generatora!
+#Zamień list comprehension na wyrażenie generatorowe (zmień odpowiednio nawiasy).
+#Przypisz wynik do zmiennej 'squares_gen'.
+#Wydrukuj za pomocą sys.getsizeof() rozmiar nowego obiektu i zobacz różnicę.
